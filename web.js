893328d09cf581d33589c60504b5c28cda52142c
EleidyH <144007961+EleidyH@users.noreply.github.com>
2024-10-09 01:08:02 +0200

    deleted files

--- web.js
+++
@@ -1,6 +0,0 @@
-// script.js
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Your message has been sent!');
-    this.reset(); // Reset the form fields
-});
